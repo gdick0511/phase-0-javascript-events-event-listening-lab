ef1f989bc1ceac494a0d6ef1001fd93298ee02f8
garrettdick <garrettd0511@gmail.com>
2021-09-11 15:14:51 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function clickArlert() {
+        alert('I was called!');
+    }
+    input.addEventListener('click',clickArlert);
 }
+
+addingEventListener(); clickArlert;
